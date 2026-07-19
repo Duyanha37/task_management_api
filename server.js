efd26a24f545bc0506914a6b8a0e0209c79211cc
DuyAnh <duyanha37@gmail.com>
2026-07-19 17:21:15 +0700

    Connect postgreSQL ok

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-const app = require('express');
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+const pool = require('./src/config/db');
+const port = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
