6f5baaebfeb125b63c4684a4f5fbc0d85b8c6d94
DuyAnh <duyanha37@gmail.com>
2026-07-20 16:34:50 +0700

    Mia postgreSQL

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ const app = express();
 const dotenv = require('dotenv').config();
 const pool = require('./src/config/db');
 const port = process.env.PORT || 3000;
+const taskRoutes = require('./src/routes/taskRoutes');
+const loginRoutes = require('./src/routes/loginRoutes');
 
-app.get('/', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM users');
-    res.json(result.rows);
-  } catch (err) {
-    console.error('Error executing query', err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+app.use(express.json());
+
+app.use('/', loginRoutes);
+
+app.use('/app', taskRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
